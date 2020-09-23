2f466e954e02484881e9572d74c5fc60f1c1fac1
Ibrahim Dagdelen <ibrahim.dagdelen@trendyol.com>
2020-09-23 12:13:30 +0300

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import OtherPage from "./OtherPage";
+import Fib from "./Fib";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page deneme </Link>
+        </div>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
